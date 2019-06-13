e53228e0aa490857dd61fc61d82ad7bbbee1eaaf
cuixiping <cuixiping@gmail.com>
2019-06-13 20:38:27 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,17 @@ const path = require('path');
 const webpcak = require('webpack');
 
 const nodeModules = {};
-/*
-fs.readdirSync('node_modules')
-    .filter(function(x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function(mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
-*/
-//['fs', 'path']
-    //.forEach(function(mod) {
-        //nodeModules[mod] = 'commonjs ' + mod;
-    //});
+Object.keys(require('./package.json').dependencies).forEach(name => {
+	nodeModules[name] = 'commonjs ' + name;
+});
 
 module.exports = {
+    context: __dirname,
     entry: [
         './proxy.js'
     ],
     output: {
+		libraryTarget: "commonjs2",
         path: __dirname,
         filename: 'proxy.bundle.js'
     },
